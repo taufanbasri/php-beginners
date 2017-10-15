861be9a6c7538cdfcea8816fc7f63b99eabb5be7
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2017-10-15 13:30:39 +0000

    Hide secret password

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  'database' => [
+    'name' => 'php_beginner',
+    'username' => 'homestead',
+    'password' => 'secret',
+    'connection' => 'mysql:host=localhost;port=33060',
+    'options' =>  [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
+    ]
+  ]
+];
--- database/Connection.php
+++ database/Connection.php
@@ -2,10 +2,15 @@
 
 class Connection
 {
-  public static function make()
+  public static function make($config)
   {
     try {
-      return new PDO('mysql:host=localhost;port=33060;dbname=php_beginner', 'homestead', 'secret');
+      return new PDO(
+        $config['connection'] . ';dbname=' . $config['name'],
+        $config['username'],
+        $config['password'],
+        $config['options']
+      );
     } catch (PDOException $e) {
       die($e->getMessage());
     }
