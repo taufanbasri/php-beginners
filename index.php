0863eed4d306aa027c7410c9174ead697fbfaad5
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2017-10-15 06:32:21 +0000

    Intro to PDO

--- index.php
+++ index.php
@@ -1,34 +1,11 @@
 <?php
 
+require 'functions.php';
+require 'Task.php';
 
-class Task
-{
-  public $description;
-  public $completed = false;
+$pdo = connectToDb();
 
-  function __construct($description)
-  {
-    $this->description = $description;
-  }
-
-  public function complete()
-  {
-    $this->completed = true;
-  }
-
-  public function isComplete()
-  {
-    return $this->completed;
-  }
-}
-
-$tasks = [
-  new Task('Learn PHP'),
-  new Task('Finish my job'),
-  new Task('Clean my room'),
-];
-
-$tasks[0]->complete();
+$tasks = fetchAllTasks($pdo);
 
 
 require 'index.view.php';
